5d5015ff2cca29de30543a9d1ce2f1520e6052bb
Jerry Eshbaugh <j2m2e2@gmail.com>
2016-02-26 17:36:32 -0500

    Update zoo connection script to connect to a ZK on another host

--- zoo.sh
+++ zoo.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-#Test
-
-IP=162.79.27.42
+HOST1=162.79.27.42
+HOST2=162.79.27.44
 PORT=2181
-TCP=tcp://$IP:$PORT
-echo $TCP
 
-if [ $HOSTNAME = "easjerrysolr2.novalocal" ]; then
-  echo "2"
-else
-  echo "1"
-fi
-
-if false ; then
-
+docker ps -a
 docker stop solr1
 docker stop solr2
 docker stop zookeeper
@@ -22,18 +12,33 @@ docker rm solr1
 docker rm solr2
 docker rm zookeeper
 
-docker run --name zookeeper -d -p 2181:2181 -p 2888:2888 -p 3888:3888 jplock/zookeeper
+if [ $HOSTNAME = "easjerrysolr.novalocal" ]; then
+  IP_PORT=$HOST1:$PORT
 
-docker run --name solr1 --link zookeeper:ZK -d -p 8983:8983 solr bash -c '/opt/solr/bin/solr start -f -z $ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT'
+  docker run --name zookeeper -d -p 2181:2181 -p 2888:2888 -p 3888:3888 jplock/zookeeper
 
-docker run --name solr2 --link zookeeper:ZK -d -p 8984:8983 solr bash -c '/opt/solr/bin/solr start -f -z $ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT'
+  docker run --name solr11 --link zookeeper:ZK -d -p 8983:8983 solr bash -c '/opt/solr/bin/solr start -f -z $ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT'
+  docker run --name solr12 --link zookeeper:ZK -d -p 8983:8983 solr bash -c '/opt/solr/bin/solr start -f -z $ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT'
 
-docker exec -i -t solr1 /opt/solr/bin/solr create_collection  -c collection1 -shards 2 -p 8983
+  docker exec -i -t solr11 /opt/solr/bin/solr create_collection  -c collection1 -shards 2 -p 8983
+  docker exec -it --user=solr solr11 bin/solr create_collection -c gettingstarted
+  docker exec -it --user=solr solr11 bin/post -c gettingstarted example/exampledocs/manufacturers.xml
 
-docker exec -it --user=solr solr1 bin/solr create_collection -c gettingstarted
+elif [ $HOSTNAME = "easjerrysolr2.novalocal" ]; then
+  IP_PORT=$HOST2:$PORT
 
-docker exec -it --user=solr solr1 bin/post -c gettingstarted example/exampledocs/manufacturers.xml
+  docker run --name solr21 --link zookeeper:ZK -d -p 8983:8983 solr bash -c '/opt/solr/bin/solr start -f -z '$IP_PORT 
+
+
+else
+  echo "Unsupported host:" $HOSTNAME
+  exit
+fi
+
+
 
 docker ps -a
 
+if true ; then
+  echo "done"
 fi
