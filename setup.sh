c35cffa03babe1c146e171b5cd46cde2a426d886
Jerry Eshbaugh <j2m2e2@gmail.com>
2016-02-26 16:40:59 -0500

    Add .gitconfig

--- setup.sh
+++ setup.sh
@@ -8,5 +8,9 @@ cp ../.bashrc ../.bashrc.bak
 rm ../.bashrc 
 ln -s DotRC/.bashrc ../.bashrc
 
+cp ../.gitconfig ../.gitconfig.bak
+rm ../.gitconfig 
+ln -s DotRC/.gitconfig ../.gitconfig
+
 # this does not work
 #source ../.bashrc
