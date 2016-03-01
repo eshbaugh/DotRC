2c4b5dcf4dac207e8cb6cc6d2ff934a2a18a827a
Jerry Eshbaugh <j2m2e2@gmail.com>
2016-03-01 18:25:08 -0500

    Fix issues with populating solr with test data

--- zoopop.sh
+++ zoopop.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-
 if [ $HOSTNAME = "easjerrysolr.novalocal" ]; then
   SOLR1=solr11
 elif [ $HOSTNAME = "easjerrysolr2.novalocal" ]; then
   SOLR1=solr21
 fi
 
-
 # create sample data
 echo "creating collection with two shards"
 
 COLL=$SOLR1'col'
 docker exec -it $SOLR1 /opt/solr/bin/solr create_collection -c $COLL -shards 2 -p 8983
+docker exec -it --user=solr $SOLR1  bin/post -c $COLL example/exampledocs/manufacturers.xml
 
 
 echo "creating EG data"
 EG=$SOLR1'eg'
-docker exec -it --user=solr $SOLR1  bin/solr create_collection -c $EG
+docker exec -it --user=solr $SOLR1  bin/solr create_collection -c $EG -p 8983
 docker exec -it --user=solr $SOLR1  bin/post -c $EG example/exampledocs/manufacturers.xml
 
 echo "done"
