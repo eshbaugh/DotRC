b4596789c99e4bf64465bd8c02a3e0a9d354f73e
Jerry Eshbaugh <j2m2e2@gmail.com>
2016-03-10 11:55:39 -0500

    Overlay networks working

--- consul.sh
+++ consul.sh
@@ -4,9 +4,5 @@
 docker rm -f consul
 
 #echo "Create keystore"
-docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h node1 --name "consul"  progrium/consul -server –bootstrap -ui-dir /ui 
-
-
-
-
+docker run -d   -p "8500:8500"   -h  "consul" --name "consul"  progrium/consul -server -bootstrap
 
