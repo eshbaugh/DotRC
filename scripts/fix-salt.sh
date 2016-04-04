4d353de945af88b2e0a1d1aebb451aa90e701028
Jerry Eshbaugh <j2m2e2@gmail.com>
2016-04-04 12:43:49 -0400

    Add pulls to make sure everything is up to date

--- scripts/fix-salt.sh
+++ scripts/fix-salt.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-# run as cloud-user
+# run as root
 
 rm -rf /srv/pillar
 rm -rf /srv/salt
@@ -9,6 +8,13 @@ chmod 777 /srv
 git clone git@github.com:eshbaugh/OC-SaltMaster.git /srv/salt
 git clone git@github.com:eshbaugh/OC-SaltPillar.git /srv/pillar
 chown -R cloud-user.cloud-user /srv
+
 cd /srv/pillar
 git checkout JerryDev 
+git pull
 
+cd /srv/salt
+git pull
+
+cd /home/cloud-user/DotRC
+git pull
